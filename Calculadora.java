bd4fde66ec615a1ad8a56ef44770768a6769f808
Mirianski <47190614+Mirianski@users.noreply.github.com>
2019-02-13 10:33:53 +0000

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+
+package ut4pruebasintesismiriam;
+
+
+public class Calculadora {
+private int operador1;
+private int operador2;
+private int operacion;
+   
+    public static void main(String[] args) {
+     
+    }
+    
+}
